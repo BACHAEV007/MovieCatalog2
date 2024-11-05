322a6551603702dc8e18633dcb388a700f19a992
mvbac <mv.bachaev@gmail.com>
2024-11-06 04:59:17 +0700

    done some refactor

--- app/src/main/java/com/example/kinoteka/presentation/MoviesDetailsScreen.kt
+++ app/src/main/java/com/example/kinoteka/presentation/MoviesDetailsScreen.kt
@@ -1,10 +1,6 @@
 package com.example.kinoteka.presentation
 
 import android.app.Activity
-import android.graphics.drawable.shapes.RoundRectShape
-import android.util.Log
-import androidx.compose.animation.core.spring
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -13,43 +9,32 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.paddingFrom
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -66,36 +51,20 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.zIndex
-import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
-import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.request.ImageRequest
 import com.example.kinoteka.R
-import com.example.kinoteka.data.datasource.TokenDataSource
-import com.example.kinoteka.data.mapper.NetworkMapper
-import com.example.kinoteka.data.network.api.FavouritesApiService
-import com.example.kinoteka.data.network.api.MovieApiService
-import com.example.kinoteka.data.network.api.RetrofitApiClient
-import com.example.kinoteka.presentation.factory.MovieDetailsViewModelFactory
-import com.example.kinoteka.presentation.mapper.MoviesMapper
 import com.example.kinoteka.presentation.viewmodel.MovieDetailsViewModel
-import okhttp3.internal.addHeaderLenient
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import com.example.kinoteka.domain.model.Author
-import com.example.kinoteka.domain.model.MovieDetails
 import com.example.kinoteka.domain.model.MovieRating
 import com.example.kinoteka.domain.model.Review
 import com.example.kinoteka.presentation.model.MovieDetailsContent
@@ -184,6 +153,7 @@ fun MoviesDetailsScreen(viewModel: MovieDetailsViewModel, activity: Activity) {
         }
     }
 }
+
 
 @Composable
 fun PosterImage(posterUrl: String, isTitleHidden: Boolean) {
